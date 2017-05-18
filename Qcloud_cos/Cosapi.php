45354f21a9a1d9516b88cc8ec5794d3a0af3fcf0
师傅不见了 <haoxioupeng@126.com>
2017-05-18 10:12:57 +0800

    Update Cosapi.php
    数组如果为空，这种回报警告

--- Qcloud_cos/Cosapi.php
+++ Qcloud_cos/Cosapi.php
@@ -471,7 +471,7 @@ class Cosapi
                 return $ret;
             }
 
-            if ($ret['data']['session']) {
+            if (isset($ret['data']['session'])) {
                 $session = $ret['data']['session'];
             }
 
